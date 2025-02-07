fd47bbf84d24255646905a5499c1d4e47e26afb4
Krushnagajare <142141511+Krushnagajare@users.noreply.github.com>
2025-02-07 15:32:48 +0530

    loops

---
+++ 05_iterations/02.js
@@ -0,0 +1,25 @@
+// while loop
+
+let index =  0;
+while(index <=10){
+    console.log(`Value of index is ${index}`);
+    index++ ;
+}
+
+let myArr = ["flash" ,"batman","superman"]
+let arr = 0;
+while(arr < myArr.length){
+    console.log(`Value is ${myArr[arr]}`);
+    arr++ ;
+}
+
+// do while loop
+
+let score = 1 ;
+
+do{
+    console.log(`Score is ${score}`);
+    score++ ;
+} while(score<=10)
+
+    
